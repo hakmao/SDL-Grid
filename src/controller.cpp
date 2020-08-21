efc65668bdb82a0bc863952f782cdb2f437de4eb
Johannes Flieger <j.flieger@gmail.com>
2020-08-21 17:27:48 +0100

    Controller class handles player input.

--- src/controller.cpp
+++ src/controller.cpp
@@ -4,5 +4,34 @@
 
 #include "controller.h"
 #include "SDL.h"
-#include "player.h"
+
+// Handle user keyboard input.
+// Change player position using WASD or arrow keys.
+void Controller::HandleInput(bool &running, Player &player) const {
+    SDL_Event event;
+    while (SDL_PollEvent(&event)) {
+        if (event.type == SDL_QUIT) {
+            running = false;
+        } else if (event.type == SDL_KEYDOWN) {
+            switch (event.key.keysym.sym) {
+                case SDLK_w:
+                case SDLK_UP:
+                    player.TryToMove(Direction::Up);
+                    break;
+                case SDLK_s:
+                case SDLK_DOWN:
+                    player.TryToMove(Direction::Down);
+                    break;
+                case SDLK_a:
+                case SDLK_LEFT:
+                    player.TryToMove(Direction::Left);
+                    break;
+                case SDLK_d:
+                case SDLK_RIGHT:
+                    player.TryToMove(Direction::Right);
+                    break;
+            }
+        }
+    }
+}
 
--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 //#include "SDL.h"
 #include "player.h"
 #include "renderer.h"
+#include "controller.h"
 
 constexpr std::size_t kgrid_cell_size = 36;
 constexpr std::size_t kgrid_width = 29;
@@ -14,10 +15,11 @@ int main()
     bool game_is_running = true;
     Player player( kgrid_width, kgrid_height);
     Renderer renderer(kgrid_cell_size, kgrid_width, kgrid_height);
+    Controller controller;
     while (game_is_running)
     {
         renderer.Render(player);
-        renderer.HandleInput(game_is_running, player);
+        controller.HandleInput(game_is_running, player);
     }
     // Goodbye message
     std::cout << "Game Over!\n";

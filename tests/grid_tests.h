fe1e5bce06b20d949ebdfa5081dd502860fc21e5
Johannes Flieger <j.flieger@gmail.com>
2020-08-24 02:52:47 +0100

    Modify test grids.

--- tests/grid_tests.h
+++ tests/grid_tests.h
@@ -15,14 +15,14 @@ namespace grid_tests
     std::size_t const sample_grid_1_size = 5 * 6;
     vector<vector<State>> const sample_grid_1 = {
             { State::Obstacle, State::Obstacle, State::Obstacle, State::Obstacle, State::Obstacle, State::Obstacle },
-            { State::Obstacle, State::Player,   State::Obstacle, State::Empty,    State::Obstacle,   State::Obstacle },
+            { State::Obstacle, State::Player,   State::Obstacle, State::Empty,    State::Empty,   State::Obstacle },
             { State::Obstacle, State::Empty,    State::Obstacle, State::Empty,    State::Obstacle, State::Obstacle },
             { State::Obstacle, State::Empty,    State::Empty,    State::Treasure,     State::Empty,    State::Obstacle },
             { State::Obstacle, State::Obstacle, State::Obstacle, State::Obstacle, State::Obstacle, State::Obstacle }};
 
     vector<vector<State>> const sample_grid_2 = {
             { State::Obstacle, State::Obstacle, State::Obstacle, State::Obstacle, State::Obstacle, State::Obstacle },
-            { State::Obstacle, State::Treasure,     State::Obstacle, State::Empty,  State::Obstacle,   State::Obstacle },
+            { State::Obstacle, State::Treasure,     State::Obstacle, State::Empty,  State::Empty,   State::Obstacle },
             { State::Obstacle, State::Empty,    State::Obstacle, State::Empty,    State::Empty,    State::Obstacle },
             { State::Obstacle, State::Player,   State::Empty,    State::Empty,    State::Empty,    State::Obstacle },
             { State::Obstacle, State::Obstacle, State::Obstacle, State::Obstacle, State::Obstacle, State::Obstacle }};

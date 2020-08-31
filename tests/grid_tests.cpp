d463fd0fef3e9967dcc00857dd4301793ca7dd21
Johannes Flieger <j.flieger@gmail.com>
2020-08-31 15:38:38 +0100

    Add additional tests for Grid class.

--- tests/grid_tests.cpp
+++ tests/grid_tests.cpp
@@ -12,51 +12,74 @@
 
 //#include <memory>
 //#include "grid.h"
+#include <tuple>
 #include "grid_tests.h"
 
 TEST_CASE("Create Grid objects from vectors of States")
 {
     Grid grid1(grid_tests::sample_grid_1);
     Grid grid2(grid_tests::sample_grid_2);
+    Grid grid3(grid_tests::sample_grid_3);
+    Grid grid4;
 
-    SECTION("Check rows and columns")
+    SECTION("Check height (rows) and width (columns)")
     {
-        CHECK(grid1.num_rows == grid_tests::sample_grid_1_rows);
-        CHECK(grid1.num_cols == grid_tests::sample_grid_1_cols);
+        CHECK(grid1.height == grid_tests::sample_grid_1_rows);
+        CHECK(grid1.width == grid_tests::sample_grid_1_cols);
         CHECK(grid1.Size() == grid_tests::sample_grid_1_size);
+        // Default settings (empty constructor)
+        CHECK(grid4.height == 23);
+        CHECK(grid4.width == 29);
+        CHECK(grid4.Size() == 29 * 23);
     }
 
     SECTION("Check valid cell positions")
     {
-        CHECK(grid1.CellIsOnGrid(0,0));
-        CHECK(grid1.CellIsOnGrid(2,3));
-        CHECK(grid1.CellIsOnGrid(4,5));
-        CHECK_FALSE(grid1.CellIsOnGrid(2,10));
-        CHECK_FALSE(grid1.CellIsOnGrid(5,4));
-        CHECK_FALSE(grid1.CellIsOnGrid(10,2));
-        CHECK_FALSE(grid1.CellIsOnGrid(-2, 10));
-        CHECK_FALSE(grid1.CellIsOnGrid(10, -2));
-        CHECK_FALSE(grid1.CellIsOnGrid(2, -3));
+        CHECK(grid1.ContainsCell(0,0));
+        CHECK(grid1.ContainsCell(2,3));
+        CHECK(grid1.ContainsCell(5,4));
+        CHECK_FALSE(grid1.ContainsCell(4,5));
+        CHECK_FALSE(grid1.ContainsCell(2,10));
+        CHECK_FALSE(grid1.ContainsCell(10,2));
+        CHECK_FALSE(grid1.ContainsCell(-2, 10));
+        CHECK_FALSE(grid1.ContainsCell(10, -2));
+        CHECK_FALSE(grid1.ContainsCell(2, -3));
     }
 
     SECTION("Equality")
     {
         CHECK(grid1 == grid1); // Sanity check
         CHECK_FALSE(grid1 == grid2);
+        CHECK_FALSE(grid2 == grid3);
+    }
+
+    SECTION("Find Player position")
+    {
+        auto [f1, x1, y1 ] = grid1.FindPlayerPosition();
+        CHECK(f1 == true);
+        CHECK(x1 == 1);
+        CHECK(y1 == 1);
+        auto [f2, x2, y2 ] = grid2.FindPlayerPosition();
+        CHECK(f2 == true);
+        CHECK(x2 == 1);
+        CHECK(y2 == 3);
+        auto [f3, x3, y3 ] = grid3.FindPlayerPosition();
+        CHECK(f3 == false);
+        CHECK(x3 == 0);
+        CHECK(y3 == 0);
     }
 }
-TEST_CASE("Write and read grid files")
+TEST_CASE("Writing and reading grid files")
 {
-    Grid grid1(grid_tests::sample_grid_1);
+    Grid grid10{grid_tests::sample_grid_1};
+    // Write to file
+    grid10.ToFile(grid_tests::test_file_1);
 
-    SECTION("Write to file")
-    {
-        CHECK(grid1.ToFile(grid_tests::testfile));
-    }
-    SECTION("Read from file")
-    {
-        Grid grid2(grid_tests::testfile);
-        CHECK( grid1 == grid2);
+    SECTION("Read from file") {
+        Grid grid11(grid_tests::test_file_1);
+        CHECK(grid10 == grid11 ); // Check that grids are equivalent
+        // Write to file
+        grid11 .ToFile(grid_tests::test_file_2);
     }
 }
 
--- tests/grid_tests.h
+++ tests/grid_tests.h
@@ -10,24 +10,32 @@
 namespace grid_tests
 {
     // Sample grids
-    std::size_t const sample_grid_1_rows = 5;
-    std::size_t const sample_grid_1_cols = 6;
+    std::size_t const sample_grid_1_rows = 5; // == height
+    std::size_t const sample_grid_1_cols = 6; // == width
     std::size_t const sample_grid_1_size = 5 * 6;
     vector<vector<State>> const sample_grid_1 = {
             { State::Obstacle, State::Obstacle, State::Obstacle, State::Obstacle, State::Obstacle, State::Obstacle },
-            { State::Obstacle, State::Player,   State::Obstacle, State::Empty,    State::Empty,   State::Obstacle },
+            { State::Obstacle, State::Player,   State::Obstacle, State::Empty,    State::Empty,    State::Obstacle },
             { State::Obstacle, State::Empty,    State::Obstacle, State::Empty,    State::Obstacle, State::Obstacle },
-            { State::Obstacle, State::Empty,    State::Empty,    State::Treasure,     State::Empty,    State::Obstacle },
+            { State::Obstacle, State::Empty,    State::Empty,    State::Treasure, State::Empty,    State::Obstacle },
             { State::Obstacle, State::Obstacle, State::Obstacle, State::Obstacle, State::Obstacle, State::Obstacle }};
 
     vector<vector<State>> const sample_grid_2 = {
             { State::Obstacle, State::Obstacle, State::Obstacle, State::Obstacle, State::Obstacle, State::Obstacle },
-            { State::Obstacle, State::Treasure,     State::Obstacle, State::Empty,  State::Empty,   State::Obstacle },
+            { State::Obstacle, State::Treasure, State::Obstacle, State::Empty,    State::Empty,    State::Obstacle },
             { State::Obstacle, State::Empty,    State::Obstacle, State::Empty,    State::Empty,    State::Obstacle },
             { State::Obstacle, State::Player,   State::Empty,    State::Empty,    State::Empty,    State::Obstacle },
             { State::Obstacle, State::Obstacle, State::Obstacle, State::Obstacle, State::Obstacle, State::Obstacle }};
 
-    // Test file name
-    std::string const testfile( "test.grid");
+    vector<vector<State>> const sample_grid_3 = {
+            { State::Obstacle, State::Obstacle, State::Obstacle, State::Obstacle, State::Obstacle, State::Obstacle },
+            { State::Obstacle, State::Treasure, State::Obstacle, State::Empty,    State::Empty,    State::Obstacle },
+            { State::Obstacle, State::Empty,    State::Empty,    State::Empty,    State::Empty,    State::Obstacle },
+            { State::Obstacle, State::Empty,    State::Empty,    State::Empty,    State::Empty,    State::Obstacle },
+            { State::Obstacle, State::Obstacle, State::Obstacle, State::Obstacle, State::Obstacle, State::Obstacle }};
+
+    // Test files
+    std::string const test_file_1( "grid_test_1.out");
+    std::string const test_file_2( "grid_test_2.out");
 }
 #endif

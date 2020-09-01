b18525bc71d9817f701cf18a52385053f85c9887
Johannes Flieger <j.flieger@gmail.com>
2020-09-01 16:14:53 +0100

    Initialise Grid from file.

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,7 @@ constexpr std::size_t kgrid_height = 23;
 int main()
 {
 //    Grid game_grid(kgrid_width, kgrid_height);
-    Grid game_grid("../tests/data/sample.grid");
+    Grid game_grid("../src/example_grid_1.grid");
     Renderer renderer(game_grid.width, game_grid.height, kgrid_cell_size);
     Controller controller;
     Game game;
